e77ec1927a95f38f82b5df776e64050826981403
Arlen marques <Arlenmarqs@gmail.com>
2025-07-25 01:16:20 -0300

    atv2

---
+++ atv3.js
@@ -0,0 +1,12 @@
+const pessoa = {
+   nome: "Lucas",
+   idade: 30,
+   cidade: "São Paulo"
+ }
+
+// Imprima o nome da pessoa.
+console.log(`Esse é o nome do usuário: ${pessoa.nome}`)
+// Imprima a idade.
+console.log(`Essa é a idade do usuário: ${pessoa.idade}`)
+// Frase de  cria
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos e nasceu em ${pessoa.cidade}`)

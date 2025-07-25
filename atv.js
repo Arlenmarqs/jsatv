57c940fe23a5182d0d06f5020d776e06f8e6df0f
Arlen marques <Arlenmarqs@gmail.com>
2025-07-25 01:15:10 -0300

    atv0

---
+++ atv.js
@@ -0,0 +1,7 @@
+let frutas = ["maçã", "banana", "laranja", "uva"];
+
+// Imprima a segunda fruta.
+console.log(`Eu gostaria de comer ${frutas[1]}`)
+// Imprima a última fruta.
+console.log(`Minha fruta preferida é ${frutas[3]}`)
+

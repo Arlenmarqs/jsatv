9f645bf7d00c30c0583e0e184ef550be1258b4a5
Arlen marques <Arlenmarqs@gmail.com>
2025-07-25 01:18:22 -0300

    atv6

---
+++ atv7.js
@@ -0,0 +1,16 @@
+ const numeros = [10, 5, 8, 3, 12, 7];
+ let x = 2
+ let soma =  numeros[0] + numeros[1]
+ let mult = numeros[2] * x 
+ let sub = numeros[3] - numeros[4]
+ let div = numeros[5] / x
+// Imprima o primeiro e o último número do array.
+console.log(`Quero ver o primeiro número: (${numeros[0]}) e o último número: (${numeros[5]})`)
+// Some o primeiro número com o segundo.
+console.log(`Some o primeiro número com o segundo: ${soma}`)
+// Multiplique o terceiro número por 2.
+console.log(`Multiplique o terceiro número por 2: ${mult}`)
+// Subtraia o quarto número do quinto.
+console.log(`Subtraia o quarto número do quinto: ${sub}`)
+// Divida o sexto número por 2.
+console.log(`Divide o sexto número por 2: ${div}`)

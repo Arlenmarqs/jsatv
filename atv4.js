7464d77575b9596da75468be24f5e514e539f360
Arlen marques <Arlenmarqs@gmail.com>
2025-07-25 01:16:47 -0300

    atv3

---
+++ atv4.js
@@ -0,0 +1,12 @@
+let linguagens = ["JavaScript", "Python", "Ruby"];
+
+// Imprima: "A primeira linguagem é JavaScript."
+console.log(`A primeira linguagem é ${linguagens[0]}.`)
+// Imprima: "Eu gosto de Python e Ruby."
+console.log(`Eu gosto de ${linguagens[1]} e ${linguagens[2]}.`)
+// Imprima todas as linguagens com vírgulas: "Linguagens: JavaScript, Python, Ruby"
+console.log(`Quero dominar todas essa linguagens: ${linguagens[0]}, ${linguagens[1]}, ${linguagens[2]}.`)
+// Imprima a frase: "Existem 3 linguagens na lista."
+console.log(`Existem 3 linguagens na lista.`)
+// Imprima : "Minhas favoritas: JavaScript | Python | Ruby"
+console.log(`MInhas linguages favoritas são: ${linguagens[0]} | ${linguagens[1]} | ${linguagens[2]}`)
